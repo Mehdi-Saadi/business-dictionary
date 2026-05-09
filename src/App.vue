50a412be43bd178f28287de67509569cbdff7815
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2026-05-10 00:09:05 +0330

    feat: implement base app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
-  <h1>app</h1>
+  <UApp>
+    <div class="min-h-dvh min-h-screen w-full flex flex-col items-center justify-center py-10"> 
+      <div class="flex flex-col space-y-5 max-w-md w-full">
+        <h1 class="text-center text-xl">
+          لغتنامه بازرگانی بین الملل
+        </h1>
+
+        <UInput 
+          size="xl" 
+          variant="subtle" 
+          placeholder="eg: Letter of Credit"
+        />
+
+        <UButton
+          v-for="n in 100"
+          :key="n"
+          :label="n"
+          size="xl" 
+          variant="subtle"
+        />
+      </div>
+    </div>
+  </UApp>
 </template>
