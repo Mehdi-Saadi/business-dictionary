3620cf5d0b35873ffe323f9728357efe0a6fdbad
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2026-05-11 17:16:54 +0330

    chore: update eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  vue: {
+    overrides: {
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 1, // max 1 attribute per line in single-line elements
+        multiline: {
+          max: 1, // max 1 attribute per line in multi-line elements
+        },
+      }],
+    },
+  },
+})
