beb7062050a952adff02a5d8718b889320ef5344
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2026-05-11 13:10:47 +0330

    chore: format file

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
 
 <template>
   <UApp>
-    <div class="min-h-dvh min-h-screen w-full flex flex-col items-center justify-center py-10"> 
+    <div class="min-h-dvh min-h-screen w-full flex flex-col items-center justify-center py-10">
       <div class="flex flex-col space-y-5 max-w-md w-full">
         <h1 class="text-center text-xl">
           لغتنامه بازرگانی بین الملل
         </h1>
 
-        <UInput 
-          size="xl" 
-          variant="subtle" 
+        <UInput
+          size="xl"
+          variant="subtle"
           placeholder="eg: Letter of Credit"
         />
 
@@ -19,7 +19,7 @@
           v-for="n in 100"
           :key="n"
           :label="n"
-          size="xl" 
+          size="xl"
           variant="subtle"
         />
       </div>
